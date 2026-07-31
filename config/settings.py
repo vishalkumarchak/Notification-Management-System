50a500edc733ec86db22d1567ff850dcb243185f
vishalkumarchak <vishalkchak15@gmail.com>
2026-08-01 02:19:43 +0530

    Save local changes

--- config/settings.py
+++ config/settings.py
@@ -97,16 +97,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'PORT': config('DB_PORT', default='5432'),
         'CONN_MAX_AGE': 600,
     }
 }
