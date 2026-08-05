6754c9c087f9588ff1d9f7f3fabd42156c5974aa
vishalkumarchak <vishalkchak15@gmail.com>
2026-08-05 15:19:05 +0530

    Move Tailwind output to root static folder

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
